6d654a39325e67dd9b68a6fabc7277564207b9cc
PA <programm-admin@users.noreply.github.com>
2025-07-14 17:15:53 +0200

    INSTALL primeNG + FIX errors + IMPLEMENT theming and color palette

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderMainComponent } from "./presentation/_components/header/header-main/header-main.component";
+import { HeaderMainComponent } from './presentation/_components/header/header-main/header-main.component';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, HeaderMainComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet, HeaderMainComponent, ButtonModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'anime-schedule-plan-frontend';
+    title = 'anime-schedule-plan-frontend';
 }
